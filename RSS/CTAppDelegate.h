e707f29490e222d885656c002245c325da27512e
Forest Belton <fbelton@theore.tc>
2013-10-10 18:32:44 -0700

    Initial Commit

---
+++ RSS/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  RSS
+//
+//  Created by Forest on 10/10/13.
+//  Copyright (c) 2013 Forest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
